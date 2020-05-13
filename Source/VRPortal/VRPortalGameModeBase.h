98ea41e03b4a42f322efa4d13b5a9e281e6a2e75
unknown <arjunmahesh97@gmail.com>
2020-05-13 22:29:18 +0530

    Character / Gun setup

---
+++ Source/VRPortal/VRPortalGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VRPortalGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRPORTAL_API AVRPortalGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
